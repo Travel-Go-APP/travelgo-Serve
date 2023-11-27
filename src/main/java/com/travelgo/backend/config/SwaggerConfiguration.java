35022d6d4f48ff57a117c422311459d0c70627dc
HwangSedong <saedongh5@naver.com>
2023-11-28 03:46:52 +0900

    Initial commit

---
+++ src/main/java/com/travelgo/backend/config/SwaggerConfiguration.java
@@ -0,0 +1,18 @@
+package com.travelgo.backend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+}
